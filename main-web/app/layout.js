5f37995530171faaeaa3679de58b557423072b8b
Thilak-c <thilak8797@gmail.com>
2026-06-23 23:43:32 +0530

    feat: integrate Facebook Pixel tracking via Next.js Script component and noscript fallback

--- main-web/app/layout.js
+++ main-web/app/layout.js
@@ -6,6 +6,7 @@ import LayoutWrapper from "../components/LayoutWrapper";
 import QueryProvider from "../components/QueryProvider";
 import { Suspense } from "react"; // Import Suspense
 import { WebsiteStructuredData, OrganizationStructuredData } from "@/components/StructuredData";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -111,10 +112,32 @@ export default function RootLayout({ children }) {
       <head>
         <WebsiteStructuredData />
         <OrganizationStructuredData />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=862293613603975&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} ${inter.variable} ${anton.variable} antialiased font-poppins`}
       >
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '862293613603975');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <QueryProvider>
           <ConvexProvider client={convex}>
             {/* <AccessGate> */}
